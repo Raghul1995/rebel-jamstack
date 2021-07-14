0a99255de76cf5e28e22805cf22d03b5d6a96ad6
Raghul Mohan <rahulmohan997@gmail.com>
2021-07-14 16:33:06 +0200

    About page

--- starter-gatsby-blog/src/pages/about.js
+++ starter-gatsby-blog/src/pages/about.js
@@ -5,9 +5,13 @@ import { Helmet } from 'react-helmet'
 import styles from './blog.module.css'
 import Layout from '../components/layout'
 
+// http://localhost:8000/___graphql
+
 class AboutIndex extends React.Component {
   render() {
     const siteTitle = 'Rebels'
+    const people = get(this, 'props.data.allContentfulPerson.edges')
+
     return (
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
@@ -16,13 +20,13 @@ class AboutIndex extends React.Component {
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
             <ul className="article-list">
-              {/* {posts.map(({ node }) => {
+              {people.map(({ node }) => {
                 return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
+                  <li key={node.id}>
+                    <h2>{node.name}</h2>
                   </li>
                 )
-              })} */}
+              })}
             </ul>
           </div>
         </div>
@@ -32,3 +36,19 @@ class AboutIndex extends React.Component {
 }
 
 export default AboutIndex
+
+export const pageQuery = graphql`
+query MyQuery {
+  allContentfulPerson {
+    edges {
+      node {
+        id
+        name
+        shortBio {
+          shortBio
+        }
+      }
+    }
+  }
+}
+`
